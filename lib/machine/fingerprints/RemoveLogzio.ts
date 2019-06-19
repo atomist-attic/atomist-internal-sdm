42917aaa6114ece45cab754937f97126f31a37d0
James Carnegie <james@atomist.com>
2019-06-19 22:25:26 +0100

    Add logzio removal fingerprint

---
+++ lib/machine/fingerprints/RemoveLogzio.ts
@@ -0,0 +1,107 @@
+/*
+ * Copyright © 2019 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+    ApplyFingerprint,
+    DiffSummaryFingerprint,
+     ExtractFingerprint,
+     Feature,
+      FP,
+      sha256,
+ } from "@atomist/sdm-pack-fingerprints";
+
+import * as R from "ramda";
+import * as xml from "xml-js";
+/**
+ * Construct an FP indicating presence of logzio configuration
+ * @return {FP}
+ */
+export function createFP(): FP {
+    return {
+        type: "logzio-presence",
+        name: "logzio-detected",
+        abbreviation: "logzio-presence",
+        version: "0.0.1",
+        data: undefined,
+        sha: sha256("logzio-detected"),
+    };
+}
+const isAppender = (e: any) => e.name === "appender" && e.attributes.class === "io.logz.logback.LogzioLogbackAppender";
+
+const getAppenders = (jsonData: any) => {
+    return R.map((e: any) => e.attributes.name,
+            R.filter(isAppender, jsonData.elements[0].elements));
+};
+
+export const createFingerprints: ExtractFingerprint = async p => {
+    const file = await p.getFile("resources/logback.xml");
+    if (file) {
+
+        const jsonData = xml.xml2js(await file.getContent());
+
+        const appenders = getAppenders(jsonData);
+        if (!R.isEmpty(appenders)) {
+            return [createFP()];
+        }
+        return undefined;
+    } else {
+        return undefined;
+    }
+};
+
+export const applyFingerprint: ApplyFingerprint = async (p, fp) => {
+    const file = await p.getFile("resources/logback.xml");
+    if (file) {
+        const jsonData = xml.xml2js(await file.getContent());
+        const appenders = getAppenders(jsonData);
+        jsonData.elements[0].elements = R.reduce((acc, e: any) => {
+            if (isAppender(e)) {
+                return acc;
+            } else if (e.name === "root") {
+                e.elements = R.filter((ee: any) => {
+                    return R.none((a: string) => {
+                        return a === ee.attributes.ref;
+                    }, appenders);
+                }, e.elements);
+                return R.append(e, acc);
+            } else {
+                return R.append(e, acc);
+            }
+        }, [], jsonData.elements[0].elements);
+        await file.setContent(xml.js2xml(jsonData, {spaces: 2}));
+        return true;
+    } else {
+        return false;
+    }
+};
+
+/* tslint:disable:max-line-length */
+export const fingerpintSummary: DiffSummaryFingerprint = (diff, target) => {
+    return {
+        title: "Logzio Configuration Found",
+        description:
+            `Logzio is deprecated and should be removed`,
+    };
+};
+
+export const LogzioPresence: Feature = {
+    displayName: "logzio config presence",
+    name: "logzio-presence",
+    extract: createFingerprints,
+    selector: fp => fp.name.startsWith(LogzioPresence.name),
+    summary: fingerpintSummary,
+    toDisplayableFingerprint: fp => fp.data,
+};
---
+++ test/RemoveLogzioFeature.test.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright © 2018 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+    InMemoryProject,
+   // logger,
+    SimpleRepoId,
+} from "@atomist/automation-client";
+import * as fs from "fs";
+import * as assert from "power-assert";
+import * as logzio from "../lib/machine/fingerprints/RemoveLogzio";
+
+describe("RemoveLogzioFeature", () => {
+
+    it("should exract fingerprints if there's a logzio appender and apply it", async () => {
+        const content = fs.readFileSync("test/logzio-logback.xml").toLocaleString();
+        const p = InMemoryProject.from(new SimpleRepoId("atomist", "sdm"),
+            { path: "resources/logback.xml", content });
+        const result = await logzio.createFingerprints(p);
+        assert.deepEqual(result, [{
+            type: "logzio-presence",
+            data: undefined,
+            name: "logzio-detected",
+             abbreviation: "logzio-presence",
+             version: "0.0.1", sha:
+             "7991b7f32d46efd8eccf66a1f1a19cbaa8449335b069b92065f72110a146e444"}]);
+        assert(true === await logzio.applyFingerprint(p, result[0]));
+        assert(fs.readFileSync("test/logzio-logback-fixed.xml").toLocaleString() === await (await p.getFile("resources/logback.xml")).getContent());
+    });
+});
